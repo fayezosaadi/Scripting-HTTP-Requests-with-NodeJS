fb5dc6fe3b135926c7653ae531d4222f49776c60
fayezosaadi <fayezosaadi@gmail.com>
2017-04-10 18:24:32 +0000

    Print out data in Chunks

---
+++ printinginchunks.js
@@ -0,0 +1,30 @@
+  var https = require('https');
+
+
+  function getAndPrintHTMLChunks () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+
+    /* Add your code here */
+      https.get(requestOptions, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received: ', data.length);
+      console.log(data.toString() + '\n');
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+      });
+  }
+
+getAndPrintHTMLChunks ();
+
+
+
