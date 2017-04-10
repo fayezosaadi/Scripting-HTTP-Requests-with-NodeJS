ccb7a7c69a87c6aa1f030a04715082c6797b139a
fayezosaadi <fayezosaadi@gmail.com>
2017-04-10 19:18:07 +0000

    Add option parameters to the function

---
+++ printingparameters.js
@@ -0,0 +1,22 @@
+  var https = require('https');
+  function getAndPrintHTML (options) {
+    https.get(options, function (response) {
+    // print out the HTTP response
+      response.setEncoding('utf8');
+      let responseString = '';
+      response.on('data', function (data) {
+        responseString += data;
+    });
+      response.on('end', function() {
+        console.log(responseString);
+        console.log('Response stream complete.');
+      });
+    });
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getAndPrintHTML (requestOptions);
